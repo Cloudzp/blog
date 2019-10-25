457a9aaf70ab8a36a112a54b01e5c1fd6b2b11c9
Your Name <you@example.com>
2019-10-25 23:28:09 +0800

    edit 2019年10月25日 23:28:09

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git commit -m "edit $(date)"
 git push origin master
 ## deploy to pro 
 scp -r source   root@www.cloudzp.club:/root/workdir/blog
-scp themes/hexo-theme-snippet/_config.yaml root@www.cloudzp.club:/root/workdir/blog/themes/hexo-theme-snippet
+scp themes/hexo-theme-snippet/_config.yml root@www.cloudzp.club:/root/workdir/blog/themes/hexo-theme-snippet
 scp -r themes/hexo-theme-snippet/source root@www.cloudzp.club:/root/workdir/blog/themes/hexo-theme-snippet
 
 echo "deploy success !"
