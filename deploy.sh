de47ee25dfd39edb74c214556cb9687b13daee92
Your Name <you@example.com>
2019-10-25 23:47:57 +0800

    edit 2019年10月25日 23:47:57

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@
 git add ./*
 git commit -m "edit $(date)"
 git push origin master
-## deploy to pro 
-scp -r source   root@www.cloudzp.club:/root/workdir/blog
-scp themes/hexo-theme-snippet/_config.yml root@www.cloudzp.club:/root/workdir/blog/themes/hexo-theme-snippet
-scp -r themes/hexo-theme-snippet/source root@www.cloudzp.club:/root/workdir/blog/themes/hexo-theme-snippet
+## deploy to pro
+ssh   root@www.cloudzp.club rm -rf /root/workdir/blog/source
+scp -C -r source   root@www.cloudzp.club:/root/workdir/blog
+scp -C themes/hexo-theme-snippet/_config.yml root@www.cloudzp.club:/root/workdir/blog/themes/hexo-theme-snippet
+scp -C -r themes/hexo-theme-snippet/source root@www.cloudzp.club:/root/workdir/blog/themes/hexo-theme-snippet
 
 echo "deploy success !"
